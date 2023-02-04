4e7c73cff24156f83344929dee9b80a7eb8666f1
Johnx69 <hoanganh692004@gmail.com>
2023-02-03 20:23:15 -0500

    update microservice

--- microservice/app.py
+++ microservice/app.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from detector import detector_blueprint
+import json
 
 #TODO: Init app
+app = Flask(__name__)
 
 #TODO: register blueprint
+app.register_blueprint(detector_blueprint)
 
+with open('config/app-config.json', 'r') as f:
+    config = json.load(f)
+    
 #TODO: Start app
+if __name__ == '__main__':
+    app.run(**config)
--- microservice/detector/controllers.py
+++ microservice/detector/controllers.py
@@ -1,6 +1,15 @@
 from detector import detector_blueprint
+from flask import request
+import os
+from app import app
+import numpy as np
+
 
 @detector_blueprint.route('/api/detect', method=['POST'])
 def detect():
-    #TODO: write detect image api
-    pass
+    # TODO: write detect image api
+    input_data = request.get_json()
+    # Convert the input data to a numpy array
+    input_data = np.array(input_data)
+    print(input_data.shape)
+    return f'Image has shape {input_data.shape}'
