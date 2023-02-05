269630a3db1aa52ce7a4e02050a632a4f92bb20f
Hiep Truong <truongminhhiep246@gmail.com>
2023-02-05 12:06:06 +0700

    Update: Read image in detector api

--- microservice/app.py
+++ microservice/app.py
@@ -1,15 +1,23 @@
-from flask import Flask
-from detector import detector_blueprint
 import json
+from flask import Flask
+from detector.controllers import detector_blueprint
 
 #TODO: Init app
 app = Flask(__name__)
 
+try:
+    with open('config/app-config.json', 'r') as f:
+        config = json.load(f)
+        config['debug'] = True if config['debug'] else False
+        config['use_reloader'] = True if config['use_reloader'] else False
+except:
+    config = {
+        'host': 'localhost',
+        'port': 3000
+    }
+
 #TODO: register blueprint
 app.register_blueprint(detector_blueprint)
-
-with open('config/app-config.json', 'r') as f:
-    config = json.load(f)
     
 #TODO: Start app
 if __name__ == '__main__':
--- microservice/detector/controllers.py
+++ microservice/detector/controllers.py
@@ -1,17 +1,15 @@
-from detector import detector_blueprint
-from flask import request
-import os
-from app import app
-import numpy as np
-from flask import jsonify
 import cv2
+import numpy as np
+from flask import jsonify, request
+from detector import detector_blueprint
 
-@detector_blueprint.route('api/detect', method=['POST'])
+@detector_blueprint.route('/api/detect', methods=['POST'])
 def detect():
     # TODO: write detect image api
     if request.files:
         binary_files = request.files.get('image_file').read()
-        img = cv2.imread(binary_files)
+        img = cv2.imdecode(np.frombuffer(binary_files, np.uint8), -1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         return f"Image has shape {img.shape}"
     
     return jsonify({
